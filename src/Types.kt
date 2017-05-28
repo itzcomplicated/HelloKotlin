e630e897edf0cdb6e0d07ea6364df77ce98f17f6
TCA <arun.cthomas3@gmail.com>
2017-05-28 18:07:41 +0530

    Types Int, Long , Double,  Float, Short

--- src/Types.kt
+++ src/Types.kt
@@ -4,13 +4,61 @@
 package  kotlintest
 
 fun main (args : Array<String>){
+
+    //No Semi colons
     //Immutable
-    val name = "Arun";
+    val name = "Arun"
     //Mutable
     var message = "Hi"
     println(message + " " + name)
 
     message = "Hello"
     println(message + " " + name)
+
+    //String Interpolation
+    println("$message $name")
+
+    //TypeInference + Int
+    var maxInt = Int.MAX_VALUE
+    var minInt = Int.MIN_VALUE
+
+    println("maxInt $maxInt")
+    println("minInt $minInt")
+
+    //TypeInference + Short
+    var maxShort = Short.MAX_VALUE
+    var minShort = Short.MIN_VALUE
+
+    println("maxShort $maxShort")
+    println("minShort $minShort")
+
+    //TypeInference + Long
+    var maxLong = Long.MAX_VALUE
+    var minLong = Long.MIN_VALUE
+
+    println("maxLong $maxLong")
+    println("minLong $minLong")
+
+
+    //TypeInference + Double
+    var maxDouble = Double.MAX_VALUE
+    var minDouble = Double.MIN_VALUE
+
+    println("maxDouble $maxDouble")
+    println("minDouble $minDouble")
+
+
+    //TypeInference + Float
+    var maxFloat = Float.MAX_VALUE
+    var minFloat = Float.MIN_VALUE
+
+    println("maxFloat $maxFloat")
+    println("minFloat $minFloat")
+
+
+
+
+
+
 
 }
